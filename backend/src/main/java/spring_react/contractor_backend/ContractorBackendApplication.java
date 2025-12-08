d65a0f2817896d74cb5f7a6c714c5033c56e4e85
yongliang <140040477+sirius-04@users.noreply.github.com>
2025-12-09 03:16:54 +0800

    backend SpringBoot setup

---
+++ backend/src/main/java/spring_react/contractor_backend/ContractorBackendApplication.java
@@ -0,0 +1,13 @@
+package spring_react.contractor_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractorBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/spring_react/contractor_backend/ContractorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_react.contractor_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
